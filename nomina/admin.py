d0eb51f58fbf43764b37a0926af0e7975f4e72d4
cerkiaDev <erickdvid32@gmail.com>
2026-03-17 18:46:32 -0500

    Se agrego todos los modelos y registros en el panel admin.

--- nomina/admin.py
+++ nomina/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Employee, Department, DeptEmp,DeptManager, Title, Salary, SalaryAuditLog, UserProfile
 
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Department)
+admin.site.register(DeptEmp)
+admin.site.register(DeptManager)
+admin.site.register(Title)
+admin.site.register(Salary)
+admin.site.register(SalaryAuditLog)
+admin.site.register(UserProfile)    
